7d48f38974cc0367a34879e4d09d6b76f0f2247a
nathanielpierson <nathanielepierson@gmail.com>
2025-06-30 11:00:37 -0400

    add cors

--- app/services/lastfm_fetcher.rb
+++ app/services/lastfm_fetcher.rb
@@ -2,20 +2,28 @@ require "uri"
 require "http"
 
 class LastfmFetcher
-  BASE_URL = "https://ws.audioscrobbler.com/2.0/"
+  BASE_URL = "http://ws.audioscrobbler.com/2.0/"
 
     def self.get_album_data(user)
+      if !user
+        render json: { error: "User parameter is requred" }
+      end
     response = HTTP.get(BASE_URL, params: {
       method: "user.getTopAlbums",
       user: user,
-      api_key: ENV["API_KEY"],
+      # period: params[:period],
+      # limit: params[:limit],
+      api_key: ENV["LASTFM_API_KEY"],
+      # shared_secret: ENV["LASTFM_SHARED_SECRET"],
       format: "json"
     })
-
-    if response.status.success?
-      JSON.parse(response.body.to_s)
-    else
-      { error: "Request failed", status: response.status }
-    end
+    p response
+    # if response.status.success?
+    #   JSON.parse(response.body.to_s)
+    # else
+    #   { error: "Request failed", status: response.status }
+    # end
   end
 end
+
+x = LastfmFetcher.get_album_data("Frogdunker")
