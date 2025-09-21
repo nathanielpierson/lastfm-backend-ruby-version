fe14df99c458e740e461c2530c38ca1353fefadc
nathanielpierson <nathanielepierson@gmail.com>
2025-09-20 21:36:20 -0400

    removed unneeded commented text from lastfm_fetcher

--- app/services/lastfm_fetcher.rb
+++ app/services/lastfm_fetcher.rb
@@ -1,7 +1,5 @@
 require "uri"
 require "http"
-# require "dotenv/load"
-# require "rails"
 require_relative "../../config/environment"
 class LastfmFetcher
   BASE_URL = "http://ws.audioscrobbler.com/2.0/"
@@ -11,12 +9,11 @@ class LastfmFetcher
       method: "user.getTopAlbums",
       user: user,
       period: period,
-      # limit: params[:limit],
+      limit: 400,
       api_key: Rails.application.credentials.dig(:api_key),
       format: "json"
     })
     p albums = response.parse
-    # p albums["topalbums"]["album"][1]["playcount"]
     # if response.status.success?
     #   JSON.parse(response.body.to_s)
     # else
